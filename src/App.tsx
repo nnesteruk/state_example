0b40ff04b6e0c68e71e1877a4c9ae9f6ae944856
nnesteruk <nikita.nesteruck57@gmail.com>
2025-08-13 17:41:50 +0300

    added ref and list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
-import Lifecycle from "./components/Lifecycle.component";
-import LifecycleFunc from "./components/LifecycleFunc.component";
+import List from "./components/List.component";
+
+export type Tasks = {
+  id: number;
+  name: string;
+};
 
 const App = () => {
-  const [show, setShow] = useState(true);
+  const [tasks, setTasks] = useState<Tasks[]>([
+    { id: 1, name: "Task 1" },
+    { id: 2, name: "Task 2" },
+    { id: 3, name: "Task 3" },
+  ]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const updateTask = (id: number) => {
+    setTasks((tasks) =>
+      tasks.map((item) =>
+        item.id === id ? { ...item, name: "!!!" + item.name } : item,
+      ),
+    );
+  };
+
+  const focusClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+    return;
+  };
 
   return (
-    <div style={{ display: "flex", gap: "20px" }}>
-      <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-        {show ? <Lifecycle /> : <LifecycleFunc />}
-        <button onClick={() => setShow(!show)}>Unmount</button>
+    <div>
+      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <input ref={inputRef} />
+        <button onClick={focusClick}>focus input</button>
       </div>
+      <List tasks={tasks} updateTask={updateTask} />
     </div>
   );
 };
---
+++ src/components/List.component.tsx
@@ -0,0 +1,25 @@
+import type { Tasks } from "../App";
+
+const List = ({
+  tasks,
+  updateTask,
+}: {
+  tasks: Tasks[];
+  updateTask: (id: number) => void;
+}) => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      {tasks.map((task) => (
+        <div
+          key={task.id}
+          style={{ display: "flex", alignItems: "center", gap: "10px" }}
+        >
+          <li>{task.name}</li>
+          <button onClick={() => updateTask(task.id)}>Изменить</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default List;
